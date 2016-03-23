6781bdc7fd53f266c27b6d9395ee676b535421d2
Keith Moore <keith.m4@gmail.com>
2016-03-23 17:01:03 -0400

    last commit before hw

--- scripts.js
+++ scripts.js
@@ -6,7 +6,9 @@ $(document).ready(function(){
 	var apiKey = "?api_key=d12bfdc8bddc77fe1e499841895aec15";
 	var configURL = baseURL + 'configuration' + apiKey;
 	var nowPlaying = baseURL + 'movie/now_playing' + apiKey;
-	var searchAll = baseURL + 'search/movie' + apiKey;
+	var allTitles = [];
+	//var searchAllURL = baseURL + "search/movie" + apiKey + "&query=" + "&page=1";
+	var searchAllURL = baseURL + "movie/id" + apiKey;
 
 	// this is typeahead.js stuff
 	var substringMatcher = function(strs) {
@@ -34,19 +36,11 @@ $(document).ready(function(){
 	  minLength: 1
 	},
 	{
-	  name: 'titles',
-	  source: substringMatcher(titles)
+	  name: 'allTitles',
+	  source: substringMatcher(allTitles)
 	});
 
 	// this is the moviedb stuff
-
-	/* function Movie(title, releaseDate, poster, popularity) {
-		this.title = title;
-		this.releaseDate = releaseDate;
-		this.poster = poster;
-		this.popularity = popularity;
-	} */
-
 	// get a base URL for poster images
 	$.getJSON(configURL, function(configData) {
 		imgBaseURL = configData.images.base_url;
@@ -66,37 +60,27 @@ $(document).ready(function(){
 		$('#poster-grid').html(newHTML);
 	});
 
-	// I want to search all the movies here.
-	/* 
-	$.getJSON(searchAll, function(allData) {
-		console.log(allData);
-		for (i=0; i<movieDataAll.results.length; i++) {
-			// var currentPoster = imgBaseURL + 'w300' + movieData.results[i].poster_path;
-			allTitles.push(movieData.results[i].title);
-		} 
-	});
-	
- 
-	function getNewReults() {
-		var searchMovies = baseURL + 'search/movie' + apiKey + '?query' + myTitle;
-		$.getJSON(searchMovies, function(movieData) {
-		titles = [];
-		newHTML = [];
-		for (i=0; i<movieData.results.length; i++) {
-			var currentPoster = imgBaseURL + 'w300' + movieData.results[i].poster_path;
-			titles.push(movieData.results[i].title);
-			newHTML += '<div class="col-sm-3">';
-			newHTML += '<img src="' + currentPoster + '">';
-			newHTML += '</div>';
-		}
-		$('#poster-grid').html(newHTML);
+	// search all the movies via typeahead...
+	console.log(searchAllURL);
+	$.getJSON(searchAllURL, function(allData) {
+		$(allData.results).each(function(){
+			allTitles.push(this.title);
 		});
-	} */
+	});
 
-	var myTitle = '';
-	$('#search').submit(function(){
-		myTitle = $('#search-str').val();
-		console.log(myTitle);
+	// pull up a specific movie poster upon clicking the search button
+	$("#search").submit(function(){
+		var searchTitle = $("#search-str").val();
+		console.log(searchTitle);
+		var movieSearchURL = baseURL + "search/movie" + apiKey + "&query=" + encodeURI(searchTitle) + "&page=1";
+		newHTML = '';
+		$.getJSON(movieSearchURL, function(movieData){
+			var movieImage = movieData.results;
+			$(movieData.results).each(function(){
+				newHTML += "<div class=' movie-poster col-sm-3'><img src=" + imgBaseURL + "w300" + this.poster_path + "'></div>";
+			});
+			$("#poster-grid").html(newHTML);
+		});
 		event.preventDefault();
 	});
 });
