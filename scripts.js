5b601bb368a898d15cf90f19e4b0a87a389365ad
Keith Moore <keith.m4@gmail.com>
2016-03-23 14:03:28 -0400

    get poster images

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
 $(document).ready(function(){
 	var baseURL = "https://api.themoviedb.org/3/";
-	var apiKey = "d12bfdc8bddc77fe1e499841895aec15";
+	var apiKey = "?api_key=d12bfdc8bddc77fe1e499841895aec15";
+	var configURL = baseURL + 'configuration' + apiKey;
+	var nowPlaying = baseURL + 'movie/now_playing' + apiKey;
 
-	var nowPlaying = baseURL + 'movie/now_playing' + '?api_key=' + apiKey;
+	function Movie(title, releaseDate, poster, popularity) {
+		this.title = title;
+		this.releaseDate = releaseDate;
+		this.poster = poster;
+		this.popularity = popularity;
+	}
+
+	var imgBaseURL;
+	$.getJSON(configURL, function(movieData) {
+		imgBaseURL = movieData.images.base_url;
+	});
+
 	$.getJSON(nowPlaying, function(movieData) {
-		// bring bag movieData from moviedb and print it to the console
-		var currResults = [];
-		for (i=0; i<4; i++) {
-			currResults[i] = movieData.results[i].title;
-			var thisOne = '#poster' + (i+1);
-			$(thisOne).html(currResults[i]);
+		var newHTML = '';
+		for (i=0; i<movieData.results.length; i++) {
+			var currentPoster = imgBaseURL + 'w300' + movieData.results[i].poster_path;
+			var currentTitle = movieData.results[i].title;
+			newHTML += '<div class="col-sm-3">';
+			newHTML += '<img src="' + currentPoster + '">';
+			newHTML += '</div>';
 		}
+		$('#poster-grid').html(newHTML);
 	});
 
 });
